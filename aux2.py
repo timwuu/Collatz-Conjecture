399258f4b334f6f2a30e7094fc5b766db8a5fcf6
timwuu <timwuu@gmail.com>
2025-02-27 16:13:50 +0800

    some fixes

--- aux2.py
+++ aux2.py
@@ -2,9 +2,14 @@
 
 steps=0
 
-input_x = input("Enter an odd number:")
+input_x = input("Enter a number:")
 
 x= int(eval(input_x))
+
+if x&1==0:
+    print(x,bin(x))
+    while x%2==0: x=x//2
+    steps += 1
 
 while x!=1:
     print(x,bin(x))
@@ -13,6 +18,7 @@ while x!=1:
 
     while x%2==0:
         x=x//2
+
 
 print(x,bin(x))
 print("- {} steps. -".format(steps))
--- aux4.py
+++ aux4.py
@@ -2,6 +2,9 @@
 #    n = p*2^(m+1) + q*(2^m-1), q in {0,1}
 #    g(n,m)= (n+1)*3^m/2^m - 1 = (2p+1)*3^m-1
 #    f(n) = p if q==0 else g(n,m)
+#
+# some simple cases:
+#    2^m-1 becomes 3^m-1
 
 steps=0
 
@@ -10,6 +13,7 @@ input_x = input("Enter a number:")
 x= int(eval(input_x))
 
 m=0
+m_sum=0
 
 #for i in range(100):
 while x!=2:
@@ -22,11 +26,14 @@ while x!=2:
             x= (x+1)*3**m-1   # 2025.02.27 timijk: x=2p
             print("m:{}".format(m))
             #print("m:{},x:{}".format(m,x))
+            m_sum+=m
             m=0 #reset m
         else:
             x=x>>1
    
     #print(x,bin(x))
+
+print("total of m:{}".format(m_sum))
 
 #print(x,bin(x))
 #print("- {} steps. -".format(steps))
